670bfeb6d71033a3bea8dbf9ae91ba2b50039456
Cheung <qichuan.zhang@qq.com>
2015-07-06 21:29:10 +0800

    v125.1
    V125.1

--- Corpration.sh
+++ Corpration.sh
@@ -37,28 +37,29 @@ info
 show
 printf "\033[?25l"
 echo -en "\t";
-endline(){
+corpration(){
+		echo -e "\n\t\t\t"
+		echo "Welcome to use CheungSSH Linux/Unix Automation Tools!"|awk  'END{a=length($0);i=1;printf "\n\n\t\t";while(i<=a){printf substr($0,i,1) "\a";i++;system("sleep  0.1")} ;"printf \n"}'
+		echo "Owner; Cheung Kei-Chuen"|awk  'END{a=length($0);i=1;printf "\n\n\t\t";while(i<=a){printf substr($0,i,1) "\a";i++;system("sleep  0.1")} ;"printf \n"}'
+		echo "Jobs: China Mobile Internet Automation Deployment"|awk  'END{a=length($0);i=1;printf "\n\n\t\t";while(i<=a){printf substr($0,i,1) "\a";i++;system("sleep  0.1")} ;"printf \n"}'
+		echo "$Corp"|awk  'END{a=length($0);i=1;printf "\n\n\t\t";while(i<=a){printf substr($0,i,1) "\a";i++;system("sleep  0.1")} ;"printf \n"}'
+		echo "QQ: 2418731289"|awk  'END{a=length($0);i=1;printf "\n\n\t\t";while(i<=a){printf substr($0,i,1) "\a";i++;system("sleep  0.1")};printf "\n" }'
+		echo "Download: http://blog.chinaunix.net/uid-29295703-id-4663051.html"|awk  'END{a=length($0);i=1;printf "\n\n\t\t";while(i<=a){printf substr($0,i,1) "\a";i++;system("sleep  0.1")};printf "\n" }'
+}
+endline1(){
 	i=0
 	printf "\033[0;98H"
 	while [ $i -le 105 ]
 	do
-       	 	printf "="
-	        printf "\033[2D"
+       	 	printf "+_"
+	        #printf "\033[2D"
+	        printf "\033[3D"
 	        let i++
-	        sleep 0.02
+	        sleep 0.03
 	done
+	printf "__"
 }
-waitt(){
-	sleep 0.1
-}
-corpration(){
-		echo -e "\n\t\t\t"
-		echo "Welcome Use CheungSSH Auto Tools!"|awk  'END{a=length($0);i=1;printf "\n\n\t\t";while(i<=a){printf substr($0,i,1) "\a";i++;system("sleep  0.1")} ;"printf \n"}'
-		echo "$Corp"|awk  'END{a=length($0);i=1;printf "\n\n\t\t";while(i<=a){printf substr($0,i,1) "\a";i++;system("sleep  0.1")} ;"printf \n"}'
-		echo "QQ: 2418731289"|awk  'END{a=length($0);i=1;printf "\n\n\t\t";while(i<=a){printf substr($0,i,1) "\a";i++;system("sleep  0.1")};printf "\n" }'
-		echo "Download http://blog.chinaunix.net/uid-29295703-id-4663051.html"|awk  'END{a=length($0);i=1;printf "\n\n\t\t";while(i<=a){printf substr($0,i,1) "\a";i++;system("sleep  0.1")};printf "\n" }'
-}
-endline
+endline1
 corpration
 printf "\n\033[?25h\n"
 stty intr ^c
