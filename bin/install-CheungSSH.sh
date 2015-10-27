b24376593c87b5ddaa5fb565b92ce7dc151c70ed
CheungSSH <qichuan.zhang@qq.com>
2015-10-27 23:51:14 +0800

    Web 2.0 update sed
    Web 2.0 update sed

--- bin/install-CheungSSH.sh
+++ bin/install-CheungSSH.sh
@@ -24,6 +24,7 @@ mkdir -p /home/cheungssh/logs
 mkdir -p /home/cheungssh/data
 mkdir -p /home/cheungssh/data/cmd/
 mkdir -p /home/cheungssh/web/cheungSSH/download/
+mkdir -p /home/cheungssh/web/cheungssh/download/
 echo "正在复制文件..."
 if [ `dirname $0` == "." ]
 then
@@ -71,8 +72,8 @@ wport=${wport:-1337}
 IP="$ip:$port"
 WIP="$ip:$wport"
 echo "正在配置.."
-sed -i  "s/112.74.205.171:800/$IP/g"   /home/cheungssh/web/cheungSSH/cheungssh/cheungSSH.html        &&
-sed -i  "s/112.74.205.171:1337/$WIP/g" /home/cheungssh/web/cheungSSH/cheungssh/cheungSSH.html     &&
+sed -i  "s/112.74.205.171:800/$IP/g"   /home/cheungssh/web/cheungssh/cheungssh/cheungSSH.html        &&
+sed -i  "s/112.74.205.171:1337/$WIP/g" /home/cheungssh/web/cheungssh/cheungssh/cheungSSH.html     &&
 sed -i  "s/1337/$wport/g" /home/cheungssh/bin/cheungssh-service.sh                                   &&
 sed -i  "s/1337/$wport/g" /home/cheungssh/bin/websocket_server_cheung.py                  
 if  [ $? -ne 0 ]
